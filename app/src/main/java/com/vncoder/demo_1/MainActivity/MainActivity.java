fbd69f21bfd99915de0fd1e1cd870e55aeac93ab
huuti <dakietkhac007@gmail.com>
2020-06-01 16:12:16 +0700

    update

--- app/src/main/java/com/vncoder/demo_1/MainActivity/MainActivity.java
+++ app/src/main/java/com/vncoder/demo_1/MainActivity/MainActivity.java
@@ -1,29 +1,83 @@
 package com.vncoder.demo_1.MainActivity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
 
 import com.vncoder.demo_1.R;
 
 public class MainActivity extends AppCompatActivity {
     Button button;
+    ImageButton btn_back;
+    EditText editTextmail;
+    EditText editText2;
+    String mailLogin;
+    String passwordLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         button = findViewById(R.id.button);
+        btn_back = findViewById(R.id.btn_back);
+        editTextmail = findViewById(R.id.editTextmail);
+        editText2 = findViewById(R.id.editText2);
+
+
+        mailLogin = editTextmail.getText().toString();
+        passwordLogin = editText2.getText().toString();
+
+        Intent intent = getIntent();
+        final String mail = intent.getStringExtra("mailInput");
+        final String password = intent.getStringExtra("passwordInput");
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
-                startActivity(intent);
+                if (mail.equals("act")){
+                    Intent intent = new Intent(MainActivity.this, Main3Activity.class);
+                    startActivity(intent);
+                }else {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                    builder.setTitle("ERROR");
+                    builder.setMessage("mail,password incorrect");
+                    builder.setCancelable(false);
+                    builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            Toast.makeText(MainActivity.this, "\n" +
+                                    "mail,password incorrect", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                    AlertDialog alertDialog = builder.create();
+                    alertDialog.show();
+                }
+
+
             }
         });
+
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainActivity.super.onBackPressed();
+            }
+        });
+
+
+
+
+
+
+
     }
 }
